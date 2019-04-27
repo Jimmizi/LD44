f5b687ac654c4a171a433458e3add22f4cf619de
Jaroslav Vozár <jarkovozar@gmail.com>
2019-04-27 15:54:25 +0200

    Added permanent upgrade scene with manager

---
+++ LD44/Assets/Script/Manager/PermanentUpgradeManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+
+public class PermanentUpgradeManager : MonoBehaviour
+{
+    public Upgrade damageUpgrade;
+    public GameObject damageUpgradeObject;
+
+    public Upgrade HPUpgrade;
+    public GameObject HPUpgradeObject;
+
+    public Upgrade cloningUpgrade;
+    public GameObject cloningUpgradeObject;
+
+    public Text cellsCounter;
+
+    private Upgrade[] upgrades;
+
+    void Start()
+    {
+        damageUpgrade = new Upgrade(damageUpgradeObject, 0);
+        HPUpgrade = new Upgrade(HPUpgradeObject, 0);
+        cloningUpgrade = new Upgrade(cloningUpgradeObject, 0);
+
+        upgrades = new Upgrade[]
+        {
+            damageUpgrade,
+            HPUpgrade,
+            cloningUpgrade
+        };
+
+        DontDestroyOnLoad(this);
+    }
+
+    void SetUpGUI(int cells)
+    {
+        foreach (Upgrade upgrade in upgrades)
+        {
+            upgrade.upgradeObject.transform.Find("StageText").GetComponent<Text>().text = "stage\n" + upgrade.stage; // sets "stage x" text
+
+            upgrade.upgradeObject.GetComponent<Button>().interactable = UpgradeCost(upgrade.stage) <= cells; // sets interactibility of upgrade button
+        }
+
+        cellsCounter.text = cells.ToString(); // sets cell counter
+    }
+
+    void Upgrade(GameObject upgradeObject)
+    {
+        Upgrade upgrade = upgrades.First(x => x.upgradeObject == upgradeObject);
+
+        upgrade.stage++;
+        upgrade.upgradeObject.transform.Find("StageText").GetComponent<Text>().text = upgrade.stage.ToString();
+        //GameManager.cells -= UpgradeCost(upgrades[upgrade]);
+        //SetUpGUI(GameManager.cells);
+    }
+
+    int UpgradeCost(int stage)
+    {
+        return stage * 5;
+    }
+}
---
+++ LD44/Assets/Script/Manager/Upgrade.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade
+{
+    public GameObject upgradeObject;
+    public int stage;
+
+    public Upgrade(GameObject upgradeObject, int stage)
+    {
+        this.upgradeObject = upgradeObject;
+        this.stage = stage;
+    }
+}
