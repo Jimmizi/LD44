9986e4e2a82a1c28ea91ad28122da3bebc903805
Gunnbju <Gunnbju@gmail.com>
2019-04-27 11:25:36 +0100

    More flow
    * goes into a placeholder upgrade screen for now

---
+++ LD44/Assets/Script/Camera/SimpleCameraLerp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraLerp : MonoBehaviour
+{
+	public Transform LerpTarget;
+	public float LerpSpeed = 5.0f;
+
+	private float _originalPositionZ;
+
+    void Start()
+    {
+	    if (LerpTarget == null)
+	    {
+		    LerpTarget = GameObject.FindGameObjectWithTag("Player")?.transform;
+	    }
+
+	    _originalPositionZ = this.transform.position.z;
+    }
+	
+    void FixedUpdate()
+    {
+        if(LerpTarget)
+        {
+	        var newPosition = Vector3.Lerp(this.transform.position, LerpTarget.position, Time.deltaTime * LerpSpeed);
+	        newPosition.z = _originalPositionZ;
+
+			this.transform.position = newPosition;
+        }
+    }
+}
---
+++ LD44/Assets/Script/_Debug/DebugPlaceholderScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugPlaceholderScene : MonoBehaviour
+{
+	public string NextSceneName;
+
+	private float _switchTimer;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+	    _switchTimer += Time.deltaTime;
+
+	    if (_switchTimer > 2.0f)
+	    {
+		    SceneManager.LoadScene(NextSceneName);
+		}
+
+    }
+}
