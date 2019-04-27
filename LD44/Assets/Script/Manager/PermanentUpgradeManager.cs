dd689a1eb5a9592933966c79f655fbab977b758e
Jaroslav Vozár <jarkovozar@gmail.com>
2019-04-27 16:22:02 +0200

    Refactor of permanent upgrades

--- LD44/Assets/Script/Manager/PermanentUpgradeManager.cs
+++ LD44/Assets/Script/Manager/PermanentUpgradeManager.cs
@@ -1,30 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
 
-
 public class PermanentUpgradeManager : MonoBehaviour
 {
-    public Upgrade damageUpgrade;
-    public GameObject damageUpgradeObject;
+    public static Upgrade damageUpgrade;   
+    public static Upgrade HPUpgrade;   
+    public static Upgrade cloningUpgrade;
 
-    public Upgrade HPUpgrade;
-    public GameObject HPUpgradeObject;
-
-    public Upgrade cloningUpgrade;
-    public GameObject cloningUpgradeObject;
-
-    public Text cellsCounter;
-
-    private Upgrade[] upgrades;
+    public static Upgrade[] upgrades;
 
     void Start()
     {
-        damageUpgrade = new Upgrade(damageUpgradeObject, 0);
-        HPUpgrade = new Upgrade(HPUpgradeObject, 0);
-        cloningUpgrade = new Upgrade(cloningUpgradeObject, 0);
+        DontDestroyOnLoad(this);
+
+        damageUpgrade = new Upgrade();
+        HPUpgrade = new Upgrade();
+        cloningUpgrade = new Upgrade();
 
         upgrades = new Upgrade[]
         {
@@ -32,33 +24,15 @@ public class PermanentUpgradeManager : MonoBehaviour
             HPUpgrade,
             cloningUpgrade
         };
-
-        DontDestroyOnLoad(this);
     }
 
-    void SetUpGUI(int cells)
-    {
-        foreach (Upgrade upgrade in upgrades)
-        {
-            upgrade.upgradeObject.transform.Find("StageText").GetComponent<Text>().text = "stage\n" + upgrade.stage; // sets "stage x" text
-
-            upgrade.upgradeObject.GetComponent<Button>().interactable = UpgradeCost(upgrade.stage) <= cells; // sets interactibility of upgrade button
-        }
-
-        cellsCounter.text = cells.ToString(); // sets cell counter
-    }
-
-    void Upgrade(GameObject upgradeObject)
-    {
-        Upgrade upgrade = upgrades.First(x => x.upgradeObject == upgradeObject);
-
-        upgrade.stage++;
-        upgrade.upgradeObject.transform.Find("StageText").GetComponent<Text>().text = upgrade.stage.ToString();
+    public static void UpgradePermanent(Upgrade upgrade)
+    {        
+        upgrade.stage++;      
         //GameManager.cells -= UpgradeCost(upgrades[upgrade]);
-        //SetUpGUI(GameManager.cells);
     }
 
-    int UpgradeCost(int stage)
+    public static int UpgradeCost(int stage)
     {
         return stage * 5;
     }
--- LD44/Assets/Script/Manager/Upgrade.cs
+++ LD44/Assets/Script/Manager/Upgrade.cs
@@ -5,11 +5,5 @@ using UnityEngine;
 public class Upgrade
 {
     public GameObject upgradeObject;
-    public int stage;
-
-    public Upgrade(GameObject upgradeObject, int stage)
-    {
-        this.upgradeObject = upgradeObject;
-        this.stage = stage;
-    }
+    public int stage = 1;
 }
