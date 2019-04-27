1747abd14cc3625079a3eb47f9f6d1b995d8fee4
Jaroslav Vozár <jarkovozar@gmail.com>
2019-04-27 16:31:08 +0200

    Added temporary upgrade manager

---
+++ LD44/Assets/Script/Manager/TemporaryUpgradeManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporaryUpgradeManager : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (Upgrade upgrade in PermanentUpgradeManager.upgrades)
+        {
+            upgrade.temporaryStage = upgrade.stage;
+        }
+    }
+
+    public void UpgradeTemporary(Upgrade upgrade)
+    {
+        upgrade.temporaryStage++;
+        //GameManager.cells -= UpgradeCost(upgrades[upgrade]);
+    }
+
+    public int UpgradeCost(int stage)
+    {
+        return stage * 2;
+    }
+}
