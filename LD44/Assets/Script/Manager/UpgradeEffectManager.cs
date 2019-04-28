41d6476292d77a16de87089ab1d54f6704c1cfd9
Jaroslav Vozár <jarkovozar@gmail.com>
2019-04-28 13:09:53 +0200

    Added basic UpgradeEffectManager for permanent upgrades

---
+++ LD44/Assets/Script/Manager/UpgradeEffectManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class UpgradeEffectManager : MonoBehaviour
+{
+    private const int HPUpgradeMultiplier = 15;
+    private const int damageUpgradeMultiplier = 4;
+    private const int cloningUpgradeMultiplier = 3;
+
+    public static void ApplyPermanentUpgrades(ActorStats actorStats)
+    {
+        actorStats.Health += PermanentUpgradeManager.HPUpgrade.CurrentStage * HPUpgradeMultiplier;
+        actorStats.Damage += PermanentUpgradeManager.damageUpgrade.CurrentStage * damageUpgradeMultiplier;
+        actorStats.Damage += PermanentUpgradeManager.cloningUpgrade.CurrentStage * cloningUpgradeMultiplier;
+    }
+}
