8c2b6e07f3975d1967cc32a2ff7df8d99b2c0fc0
ndsn <nn97s9@gmail.com>
2019-04-29 21:23:52 +0200

    Test the dynamic audio system.

---
+++ LD44/Assets/Script/Audio/AudioTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Audio;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+
+    [System.Serializable]
+    public struct TriggerObject
+    {
+        public GameObject samplerObject;
+    }
+    
+    public TriggerObject[] triggers;
+
+    public void Trigger()
+    {
+        if (triggers == null) return;
+
+        foreach (TriggerObject triggerObject in triggers)
+        {
+            Sampler sampler = triggerObject.samplerObject.GetComponent<Sampler>();
+            if (sampler != null)
+            {
+                sampler.CueWithPitch(Random.Range(0.8f, 1.8f));
+            }
+        }
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
