79f87e0a4a34b397f38d3f8aa000fc54e4d74612
Jaroslav Vozár <jarkovozar@gmail.com>
2019-04-30 01:11:38 +0200

    Small bug fix related to Pause Menu

--- LD44/Assets/PauseLevelManagerFinder.cs
+++ LD44/Assets/PauseLevelManagerFinder.cs
@@ -5,21 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class PauseLevelManagerFinder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void PauseButtonPressed()
     {
-		GameObject.FindObjectOfType<LevelManager>()?.PauseUnpause();
+		FindObjectOfType<LevelManager>()?.Unpause();
     }
 
     public void ExitButtonPressed()
